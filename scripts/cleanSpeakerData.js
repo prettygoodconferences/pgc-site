7e229d15ff70ec4f3092370997a05bfc79b2da25
Michael Cacek <michael.cacek@gmail.com>
2020-01-31 10:06:58 -0600

    chore(dependencies): update all dependencies and add prettier
    Updating all outdated dependencies and replacing xo with prettier.

--- scripts/cleanSpeakerData.js
+++ scripts/cleanSpeakerData.js
@@ -1,86 +1,107 @@
-const mapGravatar = (speaker) => {
-    if (speaker.photo.indexOf('www.gravatar.com') > -1) {
-        return {
-            ...speaker,
-            gravatar: speaker.photo
-        }
+const mapGravatar = speaker => {
+  if (speaker.photo.indexOf("www.gravatar.com") > -1) {
+    return {
+      ...speaker,
+      gravatar: speaker.photo
+    };
+  }
+
+  return speaker;
+};
+
+const mapGithub = speaker => {
+  const { github } = speaker;
+  if (github) {
+    if (github.indexOf("github.com") > -1) {
+      return {
+        ...speaker,
+        github: github
+          .substring(
+            0,
+            github
+              .split("")
+              .reverse()
+              .join("")
+              .indexOf("/")
+          )
+          .split("")
+          .reverse()
+          .join("")
+      };
     }
 
     return speaker;
+  }
+
+  return {
+    ...speaker,
+    github: undefined
+  };
 };
 
-const mapGithub = (speaker) => {
-    const {github} = speaker;
-    if (github) {
-        if (github.indexOf('github.com') > -1) {
-            return {
-                ...speaker,
-                github: github.substring(0, github.split('').reverse().join('').indexOf('/')).split('').reverse().join('')
-            };
-        }
-
-        return speaker;
+const mapTwitter = speaker => {
+  const { twitter } = speaker;
+  if (twitter) {
+    if (twitter.indexOf("twitter.com") > -1) {
+      return {
+        ...speaker,
+        twitter: twitter
+          .substring(
+            0,
+            twitter
+              .split("")
+              .reverse()
+              .join("")
+              .indexOf("/")
+          )
+          .split("")
+          .reverse()
+          .join("")
+      };
+    } else if (twitter.startsWith("@")) {
+      return {
+        ...speaker,
+        twitter: twitter.substring(1)
+      };
     }
 
-    return {
-        ...speaker,
-        github: undefined
-    };};
+    return speaker;
+  }
 
-const mapTwitter = (speaker) => {
-    const {twitter} = speaker;
-    if (twitter) {
-        if (twitter.indexOf('twitter.com') > -1) {
-            return {
-                ...speaker,
-                twitter: twitter.substring(0, twitter.split('').reverse().join('').indexOf('/')).split('').reverse().join('')
-            };
-
-        } else if (twitter.startsWith('@')) {
-            return {
-                ...speaker,
-                twitter: twitter.substring(1)
-            }
-        }
-
-        return speaker;
-    }
-
-    return {
-        ...speaker,
-        twitter: undefined
-    };
+  return {
+    ...speaker,
+    twitter: undefined
+  };
 };
 
-const mapYoutube = (speaker) => {
-    const {youtube} = speaker;
+const mapYoutube = speaker => {
+  const { youtube } = speaker;
 
-    if (youtube) {
-        return speaker;
-    }
+  if (youtube) {
+    return speaker;
+  }
 
-    return {
-        ...speaker,
-        youtube: undefined
-    };
+  return {
+    ...speaker,
+    youtube: undefined
+  };
 };
 
-const mapUnused = (speaker) => {
-    return {
-        ...speaker,
-        photo: undefined,
-        accepted: undefined,
-        communication: undefined,
-        email: undefined
-    };
-}
+const mapUnused = speaker => {
+  return {
+    ...speaker,
+    photo: undefined,
+    accepted: undefined,
+    communication: undefined,
+    email: undefined
+  };
+};
 
 const updatedSpeakers = speakers
-    .map(mapGravatar)
-    .map(mapGithub)
-    .map(mapTwitter)
-    .map(mapYoutube)
-    .map(mapUnused);
+  .map(mapGravatar)
+  .map(mapGithub)
+  .map(mapTwitter)
+  .map(mapYoutube)
+  .map(mapUnused);
 
-
-console.log(JSON.stringify(updatedSpeakers, null, '\t'));
+console.log(JSON.stringify(updatedSpeakers, null, "\t"));
